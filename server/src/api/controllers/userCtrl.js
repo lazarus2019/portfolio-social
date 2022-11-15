a815cae44fcbdecdfd0ab4e6b9c3c1aab9412cc5
NTSon <thaisonbk2020@gmail.com>
2022-11-15 08:53:53 +0700

    Fix register not redirect to login page

--- server/src/api/controllers/userCtrl.js
+++ server/src/api/controllers/userCtrl.js
@@ -37,7 +37,7 @@ const userRegisterCtrl = expressAsyncHandler(async (req, res) => {
     await checkRegisterEmail(req?.body?.email);
 
     const user = await createUser(req.body);
-    res.status(200).json({ user });
+    res.status(200).json({ status: true });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
